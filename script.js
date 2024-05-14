c6a5aa98a52566304087423e95a0c8abe17bdb75
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-14 23:52:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	let fibo1=0;
+	let fibo2=1;
+	if(num==1){
+		return foibo1;
+	}
+	else if (num==2){
+		return fibo2;
+	}
+    let fnlfibo=0;
+	for(let i=3; i<=num; i++){
+		fnlfibo=fibo1+fibo2;
+		fibo1=fibo2;
+		fibo2=fnlfibo;
+	}
+	return fnlfibo;
 }
 
 module.exports = fibonacci;
